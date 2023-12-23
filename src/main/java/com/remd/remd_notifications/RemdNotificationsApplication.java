56a20c5ea871e8215c44c68a8a25d480691a71e3
DavePhil <dave.chedjoun@facsciences-uy1.cm>
2023-12-23 09:29:53 +0100

    Initial commit

---
+++ src/main/java/com/remd/remd_notifications/RemdNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.remd.remd_notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemdNotificationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemdNotificationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/remd/remd_notifications/RemdNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remd.remd_notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemdNotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
